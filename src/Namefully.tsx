51d13e0ccdb075506b503dad2b64ad83713cd610
Ralph Florent <rflorent@mpi-bremen.de>
2020-03-23 04:33:55 +0100

    feat: define functional component

--- src/Namefully.tsx
+++ src/Namefully.tsx
@@ -5,21 +5,31 @@
  * @author Ralph Florent <ralflornt@gmail.com>
  */
 import * as React from 'react';
-import * as nf from 'namefully';
+import { Namefully, Config, Name, Nama } from 'namefully';
 
-
-class Namefully extends React.Component {
-
-    public render(): JSX.Element {
-        const name = new nf.Namefully('Ralph Florent')
-        return (
-            <div style={{textAlign: 'center'}}>
-                <h1>Namefully</h1>
-                <p> Hello, {name.fn()}!</p>
-            </div>
-        );
-    }
-
+/**
+ * @interface Props
+ */
+interface Props extends React.AllHTMLAttributes<HTMLElement> {
+    raw: string | string[] | Name[] | Nama;
+    options?: Partial<Config>;
+    method?: keyof Namefully;
+    args?: any;
+    tag?: 'div' | 'p' | 'a' | 'span';
+    children?: JSX.Element;
 }
 
-export default Namefully;
+/**
+ * Namefully, react-based component to handle person names in the Latin alphabet
+ */
+export default (props: Props): JSX.Element => {
+    const { raw, options, method, tag, children, ...htmlAttrs } = props;
+
+    const name = new Namefully(raw, options);
+
+    const Tag = tag || 'span';
+
+    return (
+        <Tag {...htmlAttrs}> {name[method || 'full']()} {children} </Tag>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,15 @@ import * as ReactDOM from 'react-dom';
 import Namefully from './Namefully';
 
 ReactDOM.render(
-    <Namefully />,
+    <Namefully
+        raw='Florent Ralph Junior'
+        options={{ orderedBy: 'lastname'}}
+        method='shorten'
+        tag='a'
+
+        href='https://github.com/ralflorent'
+        style={{color: 'blue'}}
+    />,
     document.getElementById('root')
 );
 
